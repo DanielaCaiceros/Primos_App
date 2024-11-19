b16a15ea987413e0763f3e9f728233d33ea9f424
ReginaRRJ <149405875+ReginaRRJ@users.noreply.github.com>
2024-11-19 15:29:44 -0600

    Update MiRuta.swift
    Agregar front de miura

--- PrimosApp/MiRuta.swift
+++ PrimosApp/MiRuta.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct MiRuta: View {
+    @StateObject private var actividadesModel = ActividadesModelo()
+    
     var body: some View {
-        Text("Hello, MyROUTE!")
+        NavigationView{
+            VStack{
+                
+                if !actividadesModel.actividadesRuta.isEmpty {
+                    LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 20), count: 2), spacing: 20) {
+                            ForEach(actividadesModel.actividadesRuta.prefix(2), id: \.id) { actividad in
+                            NavigationLink("hola") {
+                            Rutaview(actividadesModelo: actividadesModel, actividad: actividad)
+                            }
+                        }
+                        
+                    }
+                    .padding()
+                } else {
+                    ProgressView("Cargando tu ruta...")
+                        .padding()
+                }
+            }
+            .onAppear {
+                Task {
+                    await actividadesModel.getActividadesRuta()
+                }
+            }
+        }
+
     }
 }
 
+
+#Preview {
+    MiRuta()
+}
