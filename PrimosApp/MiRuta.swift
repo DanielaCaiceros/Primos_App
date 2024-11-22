befb9d80acfe1005efecdfae86f93eb8e8914fd5
ReginaRRJ <149405875+ReginaRRJ@users.noreply.github.com>
2024-11-22 17:28:21 -0600

    Update MiRuta.swift

--- PrimosApp/MiRuta.swift
+++ PrimosApp/MiRuta.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct MiRuta: View {
     @StateObject private var actividadesModel = ActividadesModelo()
     
@@ -50,6 +51,7 @@ struct MiRuta: View {
             }
         }
         }
+    
 }
 
 
