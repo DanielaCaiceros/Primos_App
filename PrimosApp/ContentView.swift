ce7e02ede5485a1cef4aad96b8da48f77a7cb63c
Joirid Juárez <a00835769@tec.mx>
2024-11-27 17:44:08 -0600

    ContentView corregido

--- PrimosApp/ContentView.swift
+++ PrimosApp/ContentView.swift
@@ -14,18 +14,26 @@ struct ContentView: View {
             Home()
                 .tabItem {
                     Image(systemName: "house")
+                    Text("Home")
+                        .font(.caption)
                 }
             QRScannerView()
                 .tabItem {
                     Image(systemName: "qrcode")
+                    Text("QR")
+                        .font(.caption)
                 }
             MiRuta()
                 .tabItem {
                     Image(systemName: "point.bottomleft.forward.to.point.topright.scurvepath")
+                    Text("Mi Ruta")
+                        .font(.caption)
                 }
             MapaSeleccion()
                 .tabItem {
                     Image(systemName: "map.circle.fill")
+                    Text("Mapa")
+                        .font(.caption)
                 }
         }
         .accentColor(Color(AppColors.verde))
